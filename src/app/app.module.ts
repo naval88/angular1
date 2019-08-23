9dabb1c5c0eedc1a129cdc086d5184b1fe061822
unknown <alexrozinek9@gmail.com>
2019-08-23 19:53:36 +0530

    app module file spacing issue resovled

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule,ReactiveFormsModule} from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import {NgxPaginationModule} from 'ngx-pagination';
-import {Observable} from "rxjs/index";
-
-
+import { NgxPaginationModule } from 'ngx-pagination';
+import { Observable } from "rxjs/index";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AboutusComponent } from './aboutus/aboutus.component';
